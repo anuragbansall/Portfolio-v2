d449785b4a1f6afb2df92d6d99200067d3ec68e6
Anurag Bansal <devanuragbansal@gmail.com>
2024-08-30 04:45:21 +0530

    minor changes

--- src/Components/Common/SocialCard.jsx
+++ src/Components/Common/SocialCard.jsx
@@ -4,7 +4,7 @@ function SocialCard({link, label}) {
 
   return (
     <div
-      className={`bg-[#101215] hover:bg-[#191b1e] flex justify-center items-center border border-[#373D43] p-8 text-[2rem] md:text-[3rem] rounded-2xl cursor-pointer`}
+      className={`w-full bg-[#101215] hover:bg-[#191b1e] flex justify-center items-center border border-[#373D43] p-8 text-[2rem] md:text-[3rem] rounded-2xl cursor-pointer`}
       onClick={() => {
         window.location.href = link;
       }}
